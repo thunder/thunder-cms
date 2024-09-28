8327c8ab30d6c81354a9a840eec58032ed7887ed
Daniel Bosen <daniel.bosen@burda.com>
2024-09-28 11:03:49 +0200

    Setup development, add first recipes

---
+++ README.md
@@ -0,0 +1,17 @@
+# Thunder CMS
+Thunder in recipes
+
+## Installation
+1. Install [Docker](https://docs.docker.com/get-docker/)
+2. Install ddev `brew install ddev/ddev/ddev`
+3. Clone the repository `git clone git@github.com:thunder/thunder-cms.git`
+4. Go to the project directory `cd thunder-cms`
+5. Start the project `ddev start`
+6. Install the project `ddev composer install`
+7. Install the project `ddev drush site:install recipes/thunder_cms`
+8. Open the project in the browser `ddev uli`
+
+## Update repositories in composer.json
+When adding new recipes or modules, the composer json has to be updated:
+
+    ddev setup-repositories
---
+++ modules/thunder_branding_helper/src/Plugin/Block/ThunderPoweredByBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\thunder_branding_helper\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Powered by Thunder' block.
+ *
+ * @Block(
+ *   id = "thunder_powered_by_block",
+ *   admin_label = @Translation("Powered by Thunder")
+ * )
+ */
+class ThunderPoweredByBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return ['label_display' => FALSE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    return ['#markup' => '<span>' . $this->t('Powered by <a href=":poweredby">Thunder</a>', [':poweredby' => 'http://www.thunder.org']) . '</span>'];
+  }
+
+}
